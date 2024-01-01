8f7055c760f934e43e4c494c108918da038550c3
Brian Leonardo de Morais <morais.brian360@gmail.com>
2024-01-01 19:16:56 -0300

    Ajuste

--- ProjetoExemploAspNet.Api/Program.cs
+++ ProjetoExemploAspNet.Api/Program.cs
@@ -15,11 +15,8 @@ Setup.Configure(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
